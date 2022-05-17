3488489842c46ed3ddbdac0d07a8d96744a63351
Lorreina Guyett <slorreina369@gmail.com>
2022-05-17 16:32:47 -0700

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -83,6 +83,51 @@ var endGame =  function(){
     }
 }
 
+var shop = function(){
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+
+    switch(shopOptionPrompt){
+        case "REFILL":
+        case "refill":
+            if(playerMoney >7){
+                window.alert("Refilling player's health by 20 for 7 dollars.");
+                // increase health and decrease money
+                playerHealth = playerHealth + 20;
+                playerMoney - 7;
+            }
+            else{
+                window.alert("You don't have enough money!");
+            } 
+            break;
+        case "UPGRADE":
+        case "upgrade":
+            if (playerMoney >= 7){
+                window.alert("Upgrading player's attack by 6 for 7 dollars");
+                //increase attack and decrease money
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney - 7;
+            }
+            else {
+                window.alert("You don't have enough money!");
+            }
+
+            break;
+        case "LEAVE":
+        case "leave":
+            window.alert("Leaving the store.")
+            //do nothing, so function will end
+            break;
+        default:
+            window.alert("You did not pick a valid option. Try again.");
+
+            // call shop() again to force player to pick a valid option
+            shop();
+            break;
+    }
+}
+
 var startGame = function(){
     debugger;
     // reset player stats
@@ -98,6 +143,14 @@ var startGame = function(){
             enemyHealth = 50;
             //call fight function with enemy-robot
             fight(pickedEnemyName);
+
+            if(playerHealth > 0 && i < enemyNames.length - 1){
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+                
+                if(storeConfirm){
+                    shop();
+                }
+            }
         }
         else{
             window.alert("You have lost your robot in battle! Game Over!");
