5cfdc44c51fb4d94063b3ab0333cab1114ced750
Lorreina Guyett <slorreina369@gmail.com>
2022-05-16 17:19:20 -0700

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,9 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy-robots
+//      * Fight all enemy-robots
+//      * Defeat each enemy-robot
+// "Lose" - Player robot's health is zero or less
+
 function fight(){
     window.alert("The fight has begun!");
 }
@@ -9,12 +15,12 @@ var playerMoney = 10;
 //You can also log multiple values at once like this 
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto","Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // create function
-var fight = function(){
+var fight = function(enemyName){
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!")
 
@@ -73,17 +79,27 @@ var fight = function(){
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
-
-
 };
 // execute function
-fight();
 
 console.log(playerName);
 console.log("This logs a string, good for leaving yourself a message");
+
 // this will do math and log 20
 console.log(10 + 10);
+
 //what is this?
 console.log("Our robot's " + playerName);
 var name = "Lorreina";
 console.log(name)
+
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++){
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+for(var i = 0; i < enemyNames.length; i++){
+    fight(enemyNames[i]);
+}
