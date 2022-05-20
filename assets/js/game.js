bec34e54fe733a08f37af3654bcee7cb25a5df6f
Lorreina Guyett <slorreina369@gmail.com>
2022-05-19 21:06:15 -0700

    playername null error fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,18 @@
 function fight(){
     window.alert("The fight has begun!");
 }
+var getPlayerName = function(){
+    var name = "";
+
+    while(name === "" || name === null){
+        name = prompt("What is your robot's name?");
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -150,7 +160,6 @@ var enemyInfo = [
 ];
 
 var startGame = function(){
-    //debugger;
     // reset player stats
     playerInfo.reset();
 
